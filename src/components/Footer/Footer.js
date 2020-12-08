b44b2959b0007d9e17ca05c5866cda388c1c1011
Peter Gacek <peterwgacek@gmail.com>
2020-12-08 15:12:37 -0500

    adds header and footer components

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Footer(props) {
+    return (
+        <div className="Footer">
+            <footer>
+                Footer
+                </footer>
+        </div>
+    );
+}
