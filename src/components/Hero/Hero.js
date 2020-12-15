f5c21bb4fbbfca4ad00772a0b50dcdb227a079d1
Dan Russell <dan.g.russell@gmail.com>
2020-12-15 12:16:18 -0500

    Conditional description (try Hawkeye), alt text, moved img out of ul

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -6,11 +6,10 @@ function Hero(props) {
     const { path, extension } = props.data.thumbnail
     return (
         <div className={styles.Heroes}>
+            <img src={`${path}/standard_large.${extension}`} alt={props.data.name} />
             <ul>
-                <img src={`${path}/standard_large.${extension}`} alt="hero" />
-                <li className="info">Name: {props.data.name} </li>
-                <li className="info">Description: {props.data.description} </li>
-
+                <li className="info">Name: {props.data.name}</li>
+                {props.data.description && <li className="info">Description: {props.data.description}</li>}
             </ul>
         </div>
     )
