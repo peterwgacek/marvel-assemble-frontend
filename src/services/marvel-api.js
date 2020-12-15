958f53f265f21c4792372b41846ba45038633f43
Dan Russell <dan.g.russell@gmail.com>
2020-12-15 12:14:35 -0500

    Put api key / token in seperate file to be kept out of public source control

--- src/services/marvel-api.js
+++ src/services/marvel-api.js
@@ -1,5 +1,6 @@
+import { marvelApiKey } from '../Token'
 const BASE_URI = 'https://gateway.marvel.com/v1/public';
 
 export function getHeroes(name) {
-    return fetch(`${BASE_URI}/characters?name=${name}&apikey=e7b12cf757030bbeb7dbbaa7b1ab708e`).then(res => res.json());
+    return fetch(`${BASE_URI}/characters?name=${name}&apikey=${marvelApiKey}`).then(res => res.json());
 }
