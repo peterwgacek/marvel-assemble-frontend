569359033c5b401c5b0421bed8b2198dbd689ba2
Dan Russell <dan.g.russell@gmail.com>
2020-12-15 12:15:08 -0500

    class -> className (and some formatting)

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,10 +5,10 @@ export default function Footer(props) {
     return (
 
         <footer className="Footer">
-            <a href="http://marvel.com">DATA PROVIDED BY MARVEL &copy;  {new Date().getFullYear()} </a>
+            <a href="https://marvel.com">DATA PROVIDED BY MARVEL &copy; {new Date().getFullYear()}</a>
             <div className="links">
-                <a href="https://www.linkedin.com/in/peterwgacek/"><i class="fab fa-linkedin"></i></a>
-                <a href="https://github.com/peterwgacek"><i class="fab fa-github-square"></i></a>
+                <a href="https://www.linkedin.com/in/peterwgacek/"><i className="fab fa-linkedin"></i></a>
+                <a href="https://github.com/peterwgacek"><i className="fab fa-github-square"></i></a>
             </div>
         </footer>
 
